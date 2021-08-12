69245d8c8b7eca5b2c05ecd2dca52fea8ca92dd0
ShashirajSingh <shashiraj.4323@gmail.com>
2021-08-12 15:52:04 +0530

    mongoose connection implemented

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,28 @@
 import express, { Application } from 'express';
+import mongoose from 'mongoose';
 
 const app: Application = express();
 const port: number = 3000;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+mongoose.connect(
+  `mongodb://localhost:27017/deqode-dev`,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  },
+  () => {
+    console.log('connected to mongoose...');
+  }
+);
 
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
