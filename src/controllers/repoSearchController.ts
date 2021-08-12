9a2ba05179202deff21206a8eb22ef7097eece9c
ShashirajSingh <shashiraj.4323@gmail.com>
2021-08-12 16:43:56 +0530

    Service created for repo search

--- src/controllers/repoSearchController.ts
+++ src/controllers/repoSearchController.ts
@@ -1,12 +1,13 @@
 export default class {
   static async repoSearch() {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       try {
         const result = { user_name: 'Shashi' };
 
         return resolve(result);
       } catch (error) {
         console.log(error);
+        return reject(error);
       }
     });
   }
